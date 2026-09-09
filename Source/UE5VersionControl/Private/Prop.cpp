3dc43c75ffbc52563b58966d74fff637ea697702
Subhronil Ray <sray76382@gmail.com>
2026-09-09 13:24:24 +0530

    Created Prop Blueprint.

--- Source/UE5VersionControl/Private/Prop.cpp
+++ Source/UE5VersionControl/Private/Prop.cpp
@@ -3,22 +3,18 @@
 
 #include "Prop.h"
 
-// Sets default values
 AProp::AProp()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
 }
-
-// Called when the game starts or when spawned
 void AProp::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
 void AProp::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
